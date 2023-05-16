a82e881e942a5189e07280180715835f589d38ef
Dtheran <dtheran1@gmail.com>
2023-05-16 14:44:57 -0500

    create components and pages Login and register

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    LoginFormComponent,
+    RegisterComponent,
+    RegisterFormComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
+  ],
+})
+export class AuthModule {}
